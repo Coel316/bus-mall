f55ed9f565a3bdd0fbdb1038c044ac3d328134cc
Coel Tyrell <coel316@yahoo.com>
2019-10-14 14:07:56 -0700

    got 3 pictures working

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 var leftImageEl = document.getElementById('left');
 var rightImageEl = document.getElementById('right');
+var centerImageEl = document.getElementById('center');
+
 var containerEl = document.getElementById('image_container');
 
 // leftImageEl.src = 'images/bag.jpg';
@@ -23,7 +25,10 @@ function Product(name) {
 }
 
 function makeRandom() {
-  return Math.floor(Math.random() * allProducts.length);
+  //below was --> return Math.floor(Math.random() * allProducts.length);
+  //without --> return num;
+  var num = Math.floor(Math.random() * allProducts.length);
+  return num;
 }
 
 function renderProducts() {
@@ -32,13 +37,18 @@ function renderProducts() {
   uniquePicsArray[0] = makeRandom();
   uniquePicsArray[1] = makeRandom();
 
+  //////////////////////////////////////////////////////////
+  //created a 3rd array to hold indexes
+  uniquePicsArray[2] = makeRandom();
+  //////////////////////////////////////////////////////////
+
   while (uniquePicsArray[0] === uniquePicsArray[1]) {
-    console.log('Duplicate found, Re-rolling');
+    // console.log('Duplicate found, Re-rolling');
     uniquePicsArray[1] = makeRandom();
   }
 
-// add views here
-  allProducts[uniquePicsArray[0]].views++ ;
+  // add views here
+  allProducts[uniquePicsArray[0]].views++;
 
   //get a random index
   //display a product whose index is the random number
@@ -52,6 +62,15 @@ function renderProducts() {
   rightImageEl.src = allProducts[uniquePicsArray[1]].path;
   rightImageEl.name = allProducts[uniquePicsArray[1]].name;
   rightImageEl.title = allProducts[uniquePicsArray[1]].name;
+
+  //////////////////////////////////////////////////////////
+  //add views here & 3rd picture
+  allProducts[uniquePicsArray[2]].views++;
+
+  centerImageEl.src = allProducts[uniquePicsArray[2]].path;
+  centerImageEl.name = allProducts[uniquePicsArray[2]].name;
+  centerImageEl.title = allProducts[uniquePicsArray[2]].name;
+  //////////////////////////////////////////////////////////
 }
 
 // renderProducts();
